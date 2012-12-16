a856d323e49dc9726449f37b294fb6f71f0cae2f
Alon Zakai <alonzakai@gmail.com>
2012-12-15 20:01:29 -0800

    debug

--- simulation.py
+++ simulation.py
@@ -10,11 +10,11 @@ SURVIVING = 0.5
 GENE_FITNESSES = [0.1, 0]
 
 N = 1000
-T = 10
+T = 1000
 
 class Member:
-  def __init__(self, genes=[0]*NUM_GENES):
-    self.genes = genes
+  def __init__(self, genes=None):
+    self.genes = genes or [0]*NUM_GENES
 
 class Population:
   def __init__(self, gene_ratios):
@@ -24,24 +24,26 @@ class Population:
         self.members[i].genes[j] = 1 if random.random() < gene_ratios[j] else 0
 
   def mix(self, x, y):
-    return Member([1 if random.random() < (x.genes[j]+y.genes[j])/2 else 0 for j in range(NUM_GENES)])
+    return Member([1 if random.random() <= (x.genes[j]+y.genes[j])/2.0 else 0 for j in range(NUM_GENES)])
 
   def step(self):
     fitnesses = [random.random()*RANDOM_FITNESS + sum([GENE_FITNESSES[j] * self.members[i].genes[j] for j in range(NUM_GENES)]) for i in range(N)]
     keys = range(N)
-    keys.sort(lambda x, y: int(round(fitnesses[x] - fitnesses[y])))
+    keys.sort(lambda x, y: int(round(fitnesses[y] - fitnesses[x])))
     keys = keys[:int(SURVIVING*N)]
     reps = [self.members[i] for i in keys]
     self.members = [self.mix(random.choice(reps), random.choice(reps)) for i in range(N)]
 
   def report(self):
-    g_freq = [sum([m.genes[j] for m in self.members])/NUM_GENES for j in range(NUM_GENES)]
+    g_freq = [sum([m.genes[j] for m in self.members])/float(N) for j in range(NUM_GENES)]
+    #for m in self.members: print m.genes
     print 'gene frequencies:', g_freq
 
 #
 
 p = Population([0.5, 0.5])
 for i in range(T):
+  print i,
   p.report()
   p.step()
 
